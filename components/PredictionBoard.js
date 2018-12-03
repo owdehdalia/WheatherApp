5095b9ae9a2e178055555175d450606ed80f9942
Bisan Abubaker <BisanSaeed94@gmail.com>
2018-12-03 11:11:48 +0200

    add prediction board wheather

---
+++ components/PredictionBoard.js
@@ -0,0 +1,26 @@
+
+import React, {Component} from 'react';
+import { StyleSheet,
+         Image,
+         View } from 'react-native';
+import { Text,
+         Divider,
+         Card} from 'react-native-elements';
+
+export default class PredictionBoard extends Component {
+  return (
+      <Card containerStyle={styles.cardStyle}>
+
+      </Card>
+  );
+
+}
+
+const styles = StyleSheet.create({
+
+cardStyle:{
+    backgroundColor:'blue',
+    borderRadius:20,
+    borderWidth:0
+}
+});
