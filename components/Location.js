2af42f9524fcef94f5947052024b800dceb1be3d
Bisan Abubaker <BisanSaeed94@gmail.com>
2018-12-10 15:51:43 +0200

    fix indentation

--- components/Location.js
+++ components/Location.js
@@ -25,24 +25,24 @@ export default class Location extends React.Component {
 	}
 
 	async getGeoposition() { //a method that returns the latitude and longitude of the current position
-      try{
-				await navigator.geolocation.getCurrentPosition(
-					(position) => {
-						this.setState({
-							latitude: position.coords.latitude,
-							longitude: position.coords.longitude
-						});
-						this.getForcast();
-						},
-						(error) => this.setState({ error: "Loading ..." }),
-						{ enableHighAccuracy: true, timeout: 2000, maximumAge: 2000 },
-          );
-      }
-			catch (error) {
-	      alert(' error: ' + error.message);
-	    }
-
+		try{
+			await navigator.geolocation.getCurrentPosition(
+				(position) => {
+					this.setState({
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude
+					});
+					this.getForcast();
+				},
+				(error) => this.setState({ error: "Loading ..." }),
+				{ enableHighAccuracy: true, timeout: 2000, maximumAge: 2000 },
+			);
+		}
+		catch (error) {
+			alert(' error: ' + error.message);
+		}
 	}
+
 	async getCityState(){
 		const request_url = "http://api.wunderground.com/api/" + API_KEY + "/geolookup/q/" + this.state.latitude +','+ this.state.longitude+ ".json";
 		await  axios.get(request_url).then( (response) =>{
